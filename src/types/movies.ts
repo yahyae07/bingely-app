5081abe3fb26de5397df00f4472c4e98bf76fc48
Yahya Essam <yahya.essam.elg@gmail.com>
2025-06-04 23:54:48 +0300

    Handled errors within fetching data, refactored common code and removed redundant parameters

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -8,9 +8,8 @@ export interface Movie {
   poster_path: string;
   release_date: string;
   vote_average: number;
-  rating: number;
   backdrop_path: string;
   runtime: number;
   genres: { id: number; name: string }[];
   credits: Credits;
-  }
+}
